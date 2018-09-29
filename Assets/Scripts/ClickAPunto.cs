0e1428d5a24814d1fada6d4246ce34f67a6064d7
Unknown <alberto.benavent@gmail.com>
2018-09-30 01:24:14 +0200

    Actualizado prefab del tanque ficha
    Y movidos los viejos a la carpeta "No usar"

--- Assets/Scripts/ClickAPunto.cs
+++ Assets/Scripts/ClickAPunto.cs
@@ -9,7 +9,7 @@ public class ClickAPunto : MonoBehaviour {
     Vector3 offset = new Vector3(16, 10, 0);
     Vector3 vectViewport;
     Vector3 puntoEnMapa;
-    float auxY;
+    bool semaforoMisil = true;
 
     // Use this for initialization
     void Start () {
@@ -18,15 +18,25 @@ public class ClickAPunto : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-            vectViewport = camara.ScreenToViewportPoint(Input.mousePosition);
-            puntoEnMapa = Vector3.Scale(vectViewport, ajuste) - offset;
-            auxY = puntoEnMapa.y;
-            puntoEnMapa.y = puntoEnMapa.z;
-            puntoEnMapa.z = auxY;
-            puntoEnMapa *= -1;  //Porque por algún motivo salia invertido
-            cilindro.transform.position = puntoEnMapa;
-            print(puntoEnMapa);
+		if (semaforoMisil && Input.GetMouseButtonDown(0)) {
+            StartCoroutine("LanzaMisil");
         }
 	}
+
+    IEnumerator LanzaMisil() {
+        semaforoMisil = false;
+        vectViewport = camara.ScreenToViewportPoint(Input.mousePosition);
+        puntoEnMapa = Vector3.Scale(vectViewport, ajuste) - offset;
+        float auxY = puntoEnMapa.y;
+        puntoEnMapa.y = puntoEnMapa.z;
+        puntoEnMapa.z = auxY;
+        puntoEnMapa *= -1;  //Porque por algún motivo salia invertido
+        yield return new WaitForSeconds(1.5f);
+        cilindro.transform.position = puntoEnMapa;
+        print(puntoEnMapa);
+        yield return null;
+        cilindro.transform.position = new Vector3(100, 100, 100);
+        semaforoMisil = true;
+        yield return null;
+    }
 }
