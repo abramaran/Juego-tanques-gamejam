ea33142645f8a92ba7325d67d8bf99da8cc41a57
Unknown <alberto.benavent@gmail.com>
2018-09-29 22:08:55 +0200

    Programado el daño según la distancia

--- Assets/Scripts/TanqueBasico.cs
+++ Assets/Scripts/TanqueBasico.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TanqueBasico : MonoBehaviour {
-    int health = 100;
     public int velocidad;
 
 	// Use this for initialization
---
+++ Assets/recibirDanyo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recibirDanyo : MonoBehaviour {
+
+    public int vida = 100;
+
+    void OnTriggerEnter(Collider other) {
+        Vector3 impacto;
+        float diametro = other.gameObject.transform.lossyScale.x * 100;
+
+        print("ouch!");
+
+        impacto = other.gameObject.transform.position;
+        float resta = Vector3.Distance(transform.position, impacto) * 100;
+        print("impacto: " + impacto + ", diametro: " + diametro + ", posicion: " + transform.position + "resta: " + resta);
+        print("escala: " + diametro);
+
+        if (resta > diametro / 2) {
+            print("fuera del radio");
+        } else {
+            vida -= (int)(diametro / 2 - resta);
+            print("vida: " + vida);
+        }
+
+        if (vida <= 0) Destroy(this.gameObject);
+    }
+}
