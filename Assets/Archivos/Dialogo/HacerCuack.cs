1640f59b7e0517626ec0881bad7a61dd3663feb0
Ivy <granivator@gmail.com>
2018-09-30 03:47:58 +0200

    Dialogo del tutorial casi completo

---
+++ Assets/Archivos/Dialogo/HacerCuack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HacerCuack : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().Play(); //cada vez que hagas click izquierdo reproduce esto
+        }
+	}
+}
