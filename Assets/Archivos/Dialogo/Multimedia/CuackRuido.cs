bf7ad42fe8729b3965562e30dfa593ca804f93f5
Ivy <granivator@gmail.com>
2018-09-30 09:56:05 +0200

    Dialogo con sonido

---
+++ Assets/Archivos/Dialogo/Multimedia/CuackRuido.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuackRuido : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+	}
+}
